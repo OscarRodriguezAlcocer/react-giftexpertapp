b9f1eba5834abb105626c63644fd547d303776b2
IRC. Oscar Isac Rodriguez Alcocer <35127897+OscarRodriguezAlcocer@users.noreply.github.com>
2022-03-04 15:39:12 -0600

    Update getGifs.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=uUb5J2vdJcRwQnZKPOCyTA2Usl6vwh4M`;
+  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=uUb5J2vdJcRwQnZKPOCyTA2Usl6vwh4M`;
   const resp = await fetch(url);
   const { data } = await resp.json();
   const gifs = data.map((img) => {
